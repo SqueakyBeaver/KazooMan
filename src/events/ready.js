8cef18b06d8e855881c5509aad0b6919cf7f5736
SqueakyBeaver <theboxguy659@gmail.com>
2021-09-12 17:25:22 -0500

    hopefully keep it from repeating

--- src/events/ready.js
+++ src/events/ready.js
@@ -2,7 +2,23 @@ const { Client, MessageEmbed } = require('discord.js');
 const { sendDailyMessages } = require('../daily.js');
 
 async function send_it(client) {
-    console.log('h');
+    const channel = client.channels.cache.get('875120528409632818');
+    let exit = false;
+    await channel.messages
+        .fetch()
+        .then(function (messages) {
+            sent_by_self = messages.filter(
+                (m) => m.author.id === '638201264080945162'
+            );
+            exit =
+                sent_by_self.first().createdAt.getDate() >=
+                new Date(Date.now() + 3600000 * -5).getDate();
+        })
+        .catch(console.error);
+
+    if (exit === true) {
+        return; // Don't judge me
+    }
 
     if (new Date(Date.now() + 3600000 * -5).getUTCHours() === 0) {
         sendDailyMessages(client);
