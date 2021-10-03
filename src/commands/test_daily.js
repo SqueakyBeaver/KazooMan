c3beddbd34bb04f6dbc3fc335c131b23b9aad175
SqueakyBeaver <theboxguy659@gmail.com>
2021-10-03 18:52:30 -0500

    so apparently that didn't go through?

---
+++ src/commands/test_daily.js
@@ -0,0 +1,42 @@
+const { MessageEmbed } = require('discord.js');
+const { SlashCommandBuilder } = require('@discordjs/builders');
+const { getDailyHolidays, getDailyQuote } = require('../daily.js');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('test_daily')
+        .setDescription('Test Daily stuff'),
+    async execute(interaction) {
+        send_date = new Date(Date.now() + 3600000 * -5);
+
+        const holidays = await getDailyHolidays(
+            send_date.getDate(),
+            send_date.getMonth() + 1,
+            send_date.getFullYear()
+        );
+
+        const qotd = await getDailyQuote(
+            send_date.getDate(),
+            send_date.getMonth(),
+            send_date.getFullYear()
+        );
+
+        let holiday_str = '';
+        holidays.forEach((holiday) => {
+            holiday_str += `[${holiday.name}](<${holiday.link}>)\n`;
+        });
+
+        const holidayEmbed = new MessageEmbed()
+            .setTitle(`Holidays for ${send_date.toDateString()}`)
+            .setDescription(holiday_str)
+            .setColor('RANDOM');
+
+        const quoteEmbed = new MessageEmbed()
+            .setTitle(`Quote of the day for ${send_date.toDateString()}`)
+            .setDescription(qotd.quote)
+            .setFooter(`By: ${qotd.author}`)
+            .setColor('RANDOM');
+
+        await interaction.reply({ embeds: [holidayEmbed, quoteEmbed] });
+    },
+};
