63ac817bb48d3fd7e03d2dc595e4bc170b4ddc9f
SqueakyBeaver <theboxguy659@gmail.com>
2022-06-13 15:23:36 -0500

    Fix the damn commands collection

--- src/index.ts
+++ src/index.ts
@@ -24,31 +24,32 @@ const commands = new Collection()
     .set(require('./commands/echo.js').data.name, require('./commands/echo.js'))
     .set(require('./commands/join.js').data.name, require('./commands/join.js'))
     .set(require('./commands/ping.js').data.name, require('./commands/ping.js'))
-    .set(require('./commands/report-message.js').data.name, require('./commands/report-message.js'))
+    .set('Report Message', require('./commands/report-message.js'))
     .set(require('./commands/report-slash.js').data.name, require('./commands/report-slash.js'))
     .set(require('./commands/speak.js').data.name, require('./commands/speak.js'))
     .set(require('./commands/test_daily.js').data.name, require('./commands/test_daily.js'));
 
 
 export { bot, commands };
+if (require.main === module) {
+    const eventFiles = readdirSync('gen/events').filter((file) =>
+        file.endsWith('.js')
+    );
 
-const eventFiles = readdirSync('gen/events').filter((file) =>
-    file.endsWith('.js')
-);
-
-for (const file of eventFiles) {
-    const event = require(`./events/${file}`);
-    if (event.once) {
-        bot.once(event.name, (...args) => event.execute(...args));
-    } else {
-        bot.on(event.name, (...args) => event.execute(...args));
+    for (const file of eventFiles) {
+        const event = require(`./events/${file}`);
+        if (event.once) {
+            bot.once(event.name, (...args) => event.execute(...args));
+        } else {
+            bot.on(event.name, (...args) => event.execute(...args));
+        }
     }
-}
 
-// require('./server')();
+    // require('./server')();
 
-if (token === '') {
-    bot.login(process.env.TOKEN);
-} else {
-    bot.login(token);
+    if (token === '') {
+        bot.login(process.env.TOKEN);
+    } else {
+        bot.login(token);
+    }
 }
